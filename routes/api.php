aac846d37c5162cb519368b63b0eb0efe1e30e0d
thomas@gineign.com.cn <thomas@gineign.com.cn>
2019-01-24 10:37:26 +0800

    2019-test-api-middleware

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,4 @@
 <?php
-Route::group(['prefix' => 'api/tangshi'], function() {
+Route::group(['prefix' => 'api/tangshi', 'middleware' => ['api']], function() {
     Route::get('list', '\Guozheng\TangShi\Controller\Api\ApiController@index');
 });
